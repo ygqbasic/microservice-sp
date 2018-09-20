5a850ed427f99a56208edc938e5680862f1e8c26
ygqbasic <ygqbasic@gmail.com>
2018-09-20 22:48:55 +0800

    5

---
+++ clouddo-zuul/src/main/java/com/bootdo/clouddozuul/controller/Healthz.java
@@ -0,0 +1,22 @@
+package com.bootdo.clouddozuul.controller;
+
+import com.bootdo.clouddocommon.constants.CommonConstants;
+import com.bootdo.clouddocommon.context.FilterContextHandler;
+import com.bootdo.clouddocommon.dto.MenuDTO;
+import com.bootdo.clouddozuul.prc.admin.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/")
+public class Healthz {
+    @GetMapping(path = "/healthz",produces = MediaType.TEXT_PLAIN_VALUE)
+    public String healthz(){
+        return "ok";
+    }
+}
