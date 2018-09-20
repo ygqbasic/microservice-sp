b6d8315e1c8400160f772deb918b263a95f4c46f
ygqbasic <ygqbasic@gmail.com>
2018-09-20 23:10:03 +0800

    8

--- clouddo-zuul/src/main/java/com/bootdo/clouddozuul/controller/Healthz.java
+++ clouddo-zuul/src/main/java/com/bootdo/clouddozuul/controller/Healthz.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(path = "/")
 public class Healthz {
     @GetMapping(path = "/healthz",produces = MediaType.TEXT_PLAIN_VALUE)
     public String healthz(){
